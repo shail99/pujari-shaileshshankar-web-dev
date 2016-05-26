606accc65185b635b5a32c8682e23912e1d4a448
Shailesh Pujari <shail174@ccs.neu.edu>
2016-05-25 21:26:44 -0400

    Creating an angular application and i.e. loading the angular library from cdn and app.js

---
+++ public/assignment/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by shaileshpujari on 5/25/16.
+ */
+// IIFEE Design Patthern (Immediately invoked function expression
+// (function(){})();
+(function(){
+    angular
+        .module("WebAppMaker",[]);
+
+})();
