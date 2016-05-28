82d00be7ea8a7b48166c91f4387c7c3cb782aab2
Shailesh Pujari <shail174@ccs.neu.edu>
2016-05-27 21:03:38 -0400

    get the user by id and redirect to profile page on successfull login, also set the username,firstName and lastName fields of the respective user in the profile page

--- public/assignment/views/user/user.controller.client.js
+++ public/assignment/views/user/user.controller.client.js
@@ -4,24 +4,35 @@
 (function(){
    angular
        .module("WebAppMaker")
-       .controller("LoginController",LoginController);
+       .controller("LoginController",LoginController)
+       .controller("ProfileController",ProfileController);
+
+    var users = [
+        {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
+        {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
+        {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
+        {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
+    ];
+
+    function ProfileController($routeParams){
+        var vm = this;
+
+        var id = $routeParams['id'];
+        for(var i in users){
+            if(users[i]._id === id){
+                vm.user = users[i];
+            }
+        }
+
+    }
     
-    function LoginController(){
-
-        var users = [
-            {_id: "123", username: "alice",    password: "alice",    firstName: "Alice",  lastName: "Wonder"  },
-            {_id: "234", username: "bob",      password: "bob",      firstName: "Bob",    lastName: "Marley"  },
-            {_id: "345", username: "charly",   password: "charly",   firstName: "Charly", lastName: "Garcia"  },
-            {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose",   lastName: "Annunzi" }
-        ];
-
-
+    function LoginController($location){
 
         var vm = this;
         vm.login = function(username,password){
             for(var i in users){
                 if(users[i].username === username && users[i].password === password){
-                    console.log("Yeahhhh");
+                    $location.url("/profile/"+users[i]._id);
                 }else{
                     vm.error = "Please check username and password";
                 }
